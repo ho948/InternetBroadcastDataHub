ad61127e20472709a68867d89429e0e8d8666162
ho948 <jaeho9948@gmail.com>
2025-02-12 16:24:37 +0900

    fix: superset에 맞춰 수정

--- sql/ddl/create_chzzk_table.sql
+++ sql/ddl/create_chzzk_table.sql
@@ -1,141 +1,60 @@
--- 모든 테이블 삭제
-DROP TABLE IF EXISTS chzzk.chzzk_hourly_viewers_count;
-DROP TABLE IF EXISTS chzzk.chzzk_daily_viewers_count;
-DROP TABLE IF EXISTS chzzk.chzzk_weekly_viewers_count;
-DROP TABLE IF EXISTS chzzk.chzzk_top_10_live_hourly_viewers_count;
-DROP TABLE IF EXISTS chzzk.chzzk_hourly_peak_channel;
-DROP TABLE IF EXISTS chzzk.chzzk_daily_peak_channel;
-DROP TABLE IF EXISTS chzzk.chzzk_live_reach_time_to_peak;
-DROP TABLE IF EXISTS chzzk.chzzk_channel_reach_time_to_peak;
-DROP TABLE IF EXISTS chzzk.chzzk_channel_peak_live;
-DROP TABLE IF EXISTS chzzk.chzzk_channel_followers_count;
-DROP TABLE IF EXISTS chzzk.chzzk_channel_viewers_count;
-DROP TABLE IF EXISTS chzzk.chzzk_category_hourly_viewers_count;
-DROP TABLE IF EXISTS chzzk.chzzk_category_daily_viewers_count;
+DROP TABLE IF EXISTS chzzk.popular_live_with_rank;
+DROP TABLE IF EXISTS chzzk.weekly_viewers_count;
+DROP TABLE IF EXISTS chzzk.channel_name_latest;
+DROP TABLE IF EXISTS chzzk.peak_channel;
+DROP TABLE IF EXISTS chzzk.live_time_to_peak;
+DROP TABLE IF EXISTS chzzk.channel_live_viewers_count;
+DROP TABLE IF EXISTS chzzk.channel_peak_live;
 
--- 테이블 생성
-CREATE TABLE IF NOT EXISTS chzzk.chzzk_hourly_viewers_count (
-    id SERIAL,
-    ts TIMESTAMP NOT NULL,
-    max_viewers_count INT,
-    avg_viewers_count INT,
-    total_viewers_count INT,
-    PRIMARY KEY (id, ts)
+
+CREATE TABLE IF NOT EXISTS chzzk.popular_live_with_rank (
+    execution_ts TIMESTAMP NOT NULL,
+    live_id VARCHAR(255) NOT NULL,
+    live_title VARCHAR(255),
+    viewers_count INT,
+    channel_id VARCHAR(255) NOT NULL,
+    category_type VARCHAR(255),
+    category_id VARCHAR(255),
+    category_name VARCHAR(255),
+    is_adult BOOLEAN,
+    open_ts TIMESTAMP,
+    rank INT
 );
-
-CREATE TABLE IF NOT EXISTS chzzk.chzzk_daily_viewers_count (
-    id SERIAL,
-    date DATE NOT NULL,
-    max_viewers_count INT,
-    avg_viewers_count INT,
-    total_viewers_count INT,
-    PRIMARY KEY (id, date)
-);
-
-CREATE TABLE IF NOT EXISTS chzzk.chzzk_weekly_viewers_count (
-    id SERIAL,
+CREATE TABLE IF NOT EXISTS chzzk.weekly_viewers_count (
     weekday VARCHAR(20) NOT NULL,
     max_viewers_count INT,
     avg_viewers_count INT,
-    total_viewers_count INT,
-    PRIMARY KEY (id, weekday)
+    total_viewers_count INT
 );
-
-CREATE TABLE IF NOT EXISTS chzzk.chzzk_top_10_live_hourly_viewers_count (
-    id SERIAL,
-    live_id VARCHAR(255) NOT NULL,
-    ts TIMESTAMP NOT NULL,
-    rank INT,
+CREATE TABLE IF NOT EXISTS chzzk.channel_name_latest (
+    channel_id VARCHAR(255) PRIMARY KEY,
+    channel_name VARCHAR(255),
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+);
+CREATE TABLE IF NOT EXISTS chzzk.peak_channel (
+    date DATE,
+    channel_name VARCHAR(255),
+    peak_time VARCHAR(10),
+    peak_viewers_count INT
+);
+CREATE TABLE IF NOT EXISTS chzzk.live_time_to_peak (
+    live_id VARCHAR(255) PRIMARY KEY,
+    channel_name VARCHAR(255),
+    time_to_peak INTERVAL
+);
+CREATE TABLE IF NOT EXISTS chzzk.channel_live_viewers_count (
+    channel_name VARCHAR(255),
+    live_id VARCHAR(255),
     viewers_count INT,
-    is_adult BOOLEAN,
-    PRIMARY KEY (id, live_id, ts)
+    execution_ts TIMESTAMP
 );
-
-CREATE TABLE IF NOT EXISTS chzzk.chzzk_hourly_peak_channel (
-    id SERIAL,
-    ts TIMESTAMP NOT NULL,
-    channel_id VARCHAR(255),
+CREATE TABLE IF NOT EXISTS chzzk.channel_peak_live (
     channel_name VARCHAR(255),
+    live_id VARCHAR(255),
     peak_viewers_count INT,
-    PRIMARY KEY (id, ts)
-);
-
-CREATE TABLE IF NOT EXISTS chzzk.chzzk_daily_peak_channel (
-    id SERIAL,
-    date DATE NOT NULL,
-    channel_id VARCHAR(255),
-    channel_name VARCHAR(255),
-    peak_viewers_count INT,
-    PRIMARY KEY (id, date)
-);
-
-CREATE TABLE IF NOT EXISTS chzzk.chzzk_live_reach_time_to_peak (
-    id SERIAL,
-    live_id VARCHAR(255) NOT NULL,
-    open_ts TIMESTAMP,
-    time_to_peak INTERVAL,
-    PRIMARY KEY (id, live_id)
-);
-
-CREATE TABLE IF NOT EXISTS chzzk.chzzk_channel_reach_time_to_peak (
-    id SERIAL,
-    channel_id VARCHAR(255) NOT NULL,
-    live_id VARCHAR(255) NOT NULL,
-    channel_name VARCHAR(255),
-    open_ts TIMESTAMP,
-    time_to_peak INTERVAL,
-    PRIMARY KEY (id, channel_id, live_id)
-);
-
-CREATE TABLE IF NOT EXISTS chzzk.chzzk_channel_peak_live (
-    id SERIAL,
-    channel_id VARCHAR(255) NOT NULL,
-    live_id VARCHAR(255) NOT NULL,
-    channel_name VARCHAR(255),
-    peak_ts TIMESTAMP,
-    peak_viewers_count INT,
-    live_title VARCHAR(255),
-    category_id VARCHAR(255),
-    is_adult BOOLEAN,
-    PRIMARY KEY (id, channel_id, live_id)
-);
-
-CREATE TABLE IF NOT EXISTS chzzk.chzzk_channel_followers_count (
-    id SERIAL,
-    channel_id VARCHAR(255) NOT NULL,
-    ts TIMESTAMP NOT NULL,
-    channel_name VARCHAR(255),
-    followers_count INT,
-    PRIMARY KEY (id, channel_id, ts)
-);
-
-CREATE TABLE IF NOT EXISTS chzzk.chzzk_channel_viewers_count (
-    id SERIAL,
-    channel_id VARCHAR(255) NOT NULL,
-    live_id VARCHAR(255) NOT NULL,
-    channel_name VARCHAR(255),
-    open_ts TIMESTAMP,
-    max_viewers_count INT,
-    avg_viewers_count INT,
-    PRIMARY KEY (id, channel_id, live_id)
-);
-
-CREATE TABLE IF NOT EXISTS chzzk.chzzk_category_hourly_viewers_count (
-    id SERIAL,
-    category_id VARCHAR(255) NOT NULL,
-    ts TIMESTAMP NOT NULL,
-    max_viewers_count INT,
-    avg_viewers_count INT,
-    total_viewers_count INT,
-    PRIMARY KEY (id, category_id, ts)
-);
-
-CREATE TABLE IF NOT EXISTS chzzk.chzzk_category_daily_viewers_count (
-    id SERIAL,
-    category_id VARCHAR(255) NOT NULL,
-    date TIMESTAMP NOT NULL,
-    max_viewers_count INT,
-    avg_viewers_count INT,
-    total_viewers_count INT,
-    PRIMARY KEY (id, category_id, date)
+    peak_live_title VARCHAR(255),
+    peak_category_id VARCHAR(255),
+    peak_is_adult BOOLEAN,
+    peak_ts TIMESTAMP
 );
--- sql/dml/insert_into_chzzk_table.sql
+++ sql/dml/insert_into_chzzk_table.sql
@@ -1,179 +1,114 @@
--- chzzk_hourly_viewers_count(치지직 시간대별 시청자 수) 데이터 삽입
-INSERT INTO chzzk.chzzk_hourly_viewers_count (ts, max_viewers_count, avg_viewers_count, total_viewers_count)
-SELECT 
-    DATE_TRUNC('hour', execution_ts::timestamp) AS ts,
-    MAX(viewers_count) AS max_viewers_count,
-    ROUND(AVG(viewers_count), 1) AS avg_viewers_count,
-    SUM(viewers_count) AS total_viewers_count
-FROM raw.chzzk_popular_lives
-GROUP BY DATE_TRUNC('hour', execution_ts::timestamp);
-
--- chzzk_daily_viewers_count(치지직 일별 시청자 수) 데이터 삽입
-INSERT INTO chzzk.chzzk_daily_viewers_count (date, max_viewers_count, avg_viewers_count, total_viewers_count)
-SELECT 
-    DATE(execution_ts::timestamp) AS date,
-    MAX(viewers_count) AS max_viewers_count,
-    ROUND(AVG(viewers_count), 1) AS avg_viewers_count,
-    SUM(viewers_count) AS total_viewers_count
-FROM raw.chzzk_popular_lives
-GROUP BY DATE(execution_ts::timestamp);
-
--- chzzk_weekly_viewers_count(치지직 요일별 시청자 수) 데이터 삽입
-INSERT INTO chzzk.chzzk_weekly_viewers_count (weekday, max_viewers_count, avg_viewers_count, total_viewers_count)
-SELECT 
-    TO_CHAR(execution_ts::timestamp, 'Day') AS weekday,
-    MAX(viewers_count) AS max_viewers_count,
-    ROUND(AVG(viewers_count), 1) AS avg_viewers_count,
-    SUM(viewers_count) AS total_viewers_count
-FROM raw.chzzk_popular_lives
-WHERE execution_ts::timestamp BETWEEN '2024-11-06' AND '2024-12-03'
-GROUP BY TO_CHAR(execution_ts::timestamp, 'Day');
-
--- chzzk_top_10_live_hourly_viewers_count(치지직 시간대별 탑10 라이브 시청자 수) 데이터 삽입
-INSERT INTO chzzk.chzzk_top_10_live_hourly_viewers_count (live_id, ts, rank, viewers_count, is_adult)
-WITH ranked_lives AS (
-    SELECT 
-        live_id, 
-        DATE_TRUNC('hour', execution_ts::timestamp) AS ts,
-        ROW_NUMBER() OVER (PARTITION BY DATE_TRUNC('hour', execution_ts::timestamp) ORDER BY viewers_count DESC) AS rank,
+INSERT INTO chzzk.popular_live_with_rank 
+    (execution_ts, live_id, live_title, viewers_count, channel_id, category_type, category_id, category_name, is_adult, open_ts, rank)
+    SELECT
+        execution_ts::timestamp,
+        live_id,
+        live_title,
         viewers_count,
-        is_adult
-    FROM raw.chzzk_popular_lives
-)
-SELECT live_id, ts, rank, viewers_count, is_adult
-FROM ranked_lives
-WHERE rank <= 10;
+        channel_id,
+        category_type,
+        COALESCE(category_id, 'None'),
+        category_name,
+        is_adult,
+        open_ts::timestamp,
+        ROW_NUMBER() OVER (PARTITION BY DATE_TRUNC('hour', execution_ts::timestamp) ORDER BY viewers_count DESC) AS rank
+    FROM raw.chzzk_popular_lives;
 
--- chzzk_hourly_peak_channel(치지직 시간대별 최고 시청자 수 채널) 데이터 삽입
-INSERT INTO chzzk.chzzk_hourly_peak_channel (ts, channel_id, channel_name, peak_viewers_count)
-WITH ranked_channels AS (
+INSERT INTO chzzk.channel_name_latest (channel_id, channel_name, created_at, updated_at)
     SELECT 
-        date_trunc('hour', l.execution_ts::timestamp) AS ts,
-        l.channel_id,
+        c.channel_id, 
         c.channel_name,
-        MAX(l.viewers_count) AS peak_viewers_count,
-        RANK() OVER (PARTITION BY date_trunc('hour', l.execution_ts::timestamp) ORDER BY MAX(l.viewers_count) DESC) AS rank
-    FROM raw.chzzk_popular_lives l
-    JOIN raw.chzzk_popular_channels c
-    ON l.channel_id = c.channel_id
-    GROUP BY date_trunc('hour', l.execution_ts::timestamp), l.channel_id, c.channel_name
-)
-SELECT ts, channel_id, channel_name, peak_viewers_count
-FROM ranked_channels
-WHERE rank = 1;
+        NOW() AS created_at,
+        NOW() AS updated_at
+    FROM 
+        raw.chzzk_popular_channels c,
+        (
+            SELECT channel_id, MAX(execution_ts) AS execution_ts
+            FROM raw.chzzk_popular_channels
+            GROUP BY 1
+        ) c2
+    WHERE c.channel_id = c2.channel_id AND c.execution_ts = c2.execution_ts
+    ON CONFLICT (channel_id)
+    DO UPDATE SET 
+        channel_name = EXCLUDED.channel_name,
+        updated_at = NOW();
 
--- chzzk_daily_peak_channel(치지직 일별 최고 시청자 수 채널) 데이터 삽입
-INSERT INTO chzzk.chzzk_daily_peak_channel (date, channel_id, channel_name, peak_viewers_count)
-WITH ranked_channels AS (
+INSERT INTO chzzk.peak_channel (date, channel_name, peak_time, peak_viewers_count)
+    WITH ranked_data AS (
+        SELECT 
+            DATE(execution_ts) AS date,
+            channel_id,
+            TO_CHAR(execution_ts, 'HH24:MI') AS peak_time,
+            viewers_count AS peak_viewers_count,
+            RANK() OVER (PARTITION BY DATE(execution_ts) ORDER BY viewers_count DESC) AS rnk
+        FROM chzzk.popular_live_with_rank
+    )
     SELECT 
-        date_trunc('day', l.execution_ts::timestamp) AS date,
-        l.channel_id,
-        c.channel_name,
-        MAX(l.viewers_count) AS peak_viewers_count,
-        RANK() OVER (PARTITION BY date_trunc('day', l.execution_ts::timestamp) ORDER BY MAX(l.viewers_count) DESC) AS rank
-    FROM raw.chzzk_popular_lives l
-    JOIN raw.chzzk_popular_channels c
-    ON l.channel_id = c.channel_id
-    GROUP BY date_trunc('day', l.execution_ts::timestamp), l.channel_id, c.channel_name
-)
-SELECT date, channel_id, channel_name, peak_viewers_count
-FROM ranked_channels
-WHERE rank = 1;
+        r.date AS date,
+        c.channel_name AS channel_name,
+        r.peak_time AS peak_time,
+        r.peak_viewers_count AS peak_viewers_count
+    FROM ranked_data r
+    JOIN chzzk.channel_name_latest c ON r.channel_id = c.channel_id
+    WHERE rnk = 1;
 
--- chzzk_live_reach_time_to_peak(치지직 라이브별 최대 시청자 수 도달 시간) 데이터 삽입
-INSERT INTO chzzk.chzzk_live_reach_time_to_peak (live_id, open_ts, time_to_peak)
-SELECT 
-    l.live_id,
-    l.open_ts::timestamp,
-    (l.execution_ts::timestamp - l.open_ts::timestamp) AS time_to_peak
-FROM raw.chzzk_popular_lives l
-JOIN (
-    SELECT live_id, MAX(viewers_count) AS max_viewers_count
-    FROM raw.chzzk_popular_lives
-    GROUP BY live_id
-) max_viewers
-ON l.live_id = max_viewers.live_id
-AND l.viewers_count = max_viewers.max_viewers_count;
+INSERT INTO chzzk.live_time_to_peak (live_id, channel_name, time_to_peak)
+    WITH live_max_viewers AS (
+        SELECT live_id, MAX(viewers_count) AS max_viewers_count
+        FROM chzzk.popular_live_with_rank
+        GROUP BY 1
+    )
+    SELECT l.live_id, channel_name, (MIN(execution_ts) - open_ts) AS time_to_peak
+    FROM chzzk.popular_live_with_rank l
+    JOIN live_max_viewers lm ON l.live_id = lm.live_id AND l.viewers_count = lm.max_viewers_count
+    JOIN chzzk.channel_name_latest c ON l.channel_id = c.channel_id
+    WHERE open_ts < execution_ts
+    GROUP BY l.live_id, channel_name, open_ts;
 
--- chzzk_channel_reach_time_to_peak(치지직 채널별 각 라이브의 최대 시청자 수 도달 시간) 데이터 삽입
-INSERT INTO chzzk.chzzk_channel_reach_time_to_peak (channel_id, live_id, channel_name, open_ts, time_to_peak)
-SELECT DISTINCT
-    l.channel_id,
-    l.live_id,
-    c.channel_name,
-    l.open_ts::timestamp,
-    (l.execution_ts::timestamp - l.open_ts::timestamp) AS time_to_peak
-FROM raw.chzzk_popular_lives l
-JOIN raw.chzzk_popular_channels c
-    ON l.channel_id = c.channel_id
-JOIN (
-    SELECT live_id, MAX(viewers_count) AS max_viewers_count
-    FROM raw.chzzk_popular_lives
-    GROUP BY live_id
-) max_viewers
-    ON l.live_id = max_viewers.live_id
-    AND l.viewers_count = max_viewers.max_viewers_count;
-    
--- chzzk_channel_peak_live(치지직 채널별 최대 시청자 수 도달 시점의 라이브 정보)
-INSERT INTO chzzk.chzzk_channel_peak_live (channel_id, live_id, channel_name, peak_ts, peak_viewers_count, live_title, category_id, is_adult)
-SELECT DISTINCT
-    l.channel_id,
-    l.live_id,
-    c.channel_name,
-    l.execution_ts::timestamp AS peak_ts,
-    l.viewers_count AS peak_viewers_count,
-    l.live_title,
-    l.category_id,
-    l.is_adult
-FROM raw.chzzk_popular_lives l
-JOIN raw.chzzk_popular_channels c ON l.channel_id = c.channel_id
-WHERE l.viewers_count = (
-     SELECT MAX(viewers_count)
-     FROM raw.chzzk_popular_lives 
-     WHERE live_id = l.live_id
-    );
+INSERT INTO chzzk.channel_live_viewers_count (channel_name, live_id, viewers_count, execution_ts)
+    SELECT c.channel_name, live_id, viewers_count, execution_ts
+    FROM chzzk.popular_live_with_rank l
+    JOIN chzzk.channel_name_latest c ON l.channel_id = c.channel_id
+    ORDER BY execution_ts;
 
--- chzzk_channel_followers_count(치지직 채널별 팔로워 수) 데이터 삽입
-INSERT INTO chzzk.chzzk_channel_followers_count (channel_id, ts, channel_name, followers_count)
-SELECT 
-    channel_id,
-    execution_ts::timestamp AS ts,
-    channel_name,
-    followers_count
-FROM raw.chzzk_popular_channels;
+DROP TABLE IF EXISTS chzzk.channel_peak_live;
+CREATE TABLE IF NOT EXISTS chzzk.channel_peak_live (
+    channel_name VARCHAR(255),
+    live_id VARCHAR(255),
+    peak_viewers_count INT,
+    peak_live_title VARCHAR(255),
+    peak_category_id VARCHAR(255),
+    peak_is_adult BOOLEAN,
+    peak_ts TIMESTAMP
+);
 
--- chzzk_channel_viewers_count(치지직 채널별 각 라이브 방송의 시청자 수) 데이터 삽입
-INSERT INTO chzzk.chzzk_channel_viewers_count (channel_id, live_id, channel_name, open_ts, max_viewers_count, avg_viewers_count)
-SELECT 
-    l.channel_id,
-    l.live_id,
-    c.channel_name,
-    l.open_ts::timestamp,
-    MAX(l.viewers_count) AS max_viewers_count,
-    ROUND(AVG(l.viewers_count), 1) AS avg_viewers_count
-FROM raw.chzzk_popular_lives l
-JOIN raw.chzzk_popular_channels c
-    ON l.channel_id = c.channel_id
-GROUP BY l.channel_id, l.live_id, c.channel_name, l.open_ts::timestamp;
-
--- chzzk_category_hourly_viewers_count(치지직 카테고리별 각 시간대의 시청자 수) 데이터 삽입
-INSERT INTO chzzk.chzzk_category_hourly_viewers_count (category_id, ts, max_viewers_count, avg_viewers_count, total_viewers_count)
-SELECT 
-    COALESCE(l.category_id, 'None'),
-    DATE_TRUNC('hour', l.execution_ts::timestamp) AS ts,
-    MAX(l.viewers_count) AS max_viewers_count,
-    ROUND(AVG(l.viewers_count), 1) AS avg_viewers_count,
-    SUM(l.viewers_count) AS total_viewers_count
-FROM raw.chzzk_popular_lives l
-GROUP BY l.category_id, DATE_TRUNC('hour', l.execution_ts::timestamp);
-
--- chzzk_category_daily_viewers_count(치지직 카테고리별 각 일자의 시청자 수) 데이터 삽입
-INSERT INTO chzzk.chzzk_category_daily_viewers_count (category_id, date, max_viewers_count, avg_viewers_count, total_viewers_count)
-SELECT 
-    COALESCE(l.category_id, 'None'),
-    DATE_TRUNC('day', l.execution_ts::timestamp) AS date,
-    MAX(l.viewers_count) AS max_viewers_count,
-    ROUND(AVG(l.viewers_count), 1) AS avg_viewers_count,
-    SUM(l.viewers_count) AS total_viewers_count
-FROM raw.chzzk_popular_lives l
-GROUP BY l.category_id, DATE_TRUNC('day', l.execution_ts::timestamp);
+INSERT INTO chzzk.channel_peak_live 
+    (channel_name, live_id, peak_viewers_count, peak_live_title, peak_category_id, peak_is_adult, peak_ts)
+  SELECT 
+      cn.channel_name,
+      pl.live_id,
+      pl.viewers_count AS peak_viewers_count,
+      pl.live_title AS peak_live_title,
+      pl.category_id AS peak_category_id,
+      pl.is_adult AS peak_is_adult,
+      MIN(pl.execution_ts) AS peak_ts
+  FROM (
+      SELECT 
+          channel_id,
+          live_id, 
+          viewers_count, 
+          live_title, 
+          category_id, 
+          is_adult, 
+          execution_ts
+      FROM chzzk.popular_live_with_rank pl
+      WHERE (pl.live_id, pl.viewers_count) IN (
+          SELECT 
+              live_id, 
+              MAX(viewers_count)
+          FROM chzzk.popular_live_with_rank
+          GROUP BY live_id
+      )
+  ) pl
+  JOIN chzzk.channel_name_latest cn ON pl.channel_id = cn.channel_id
+  GROUP BY 1, 2, 3, 4, 5, 6;
