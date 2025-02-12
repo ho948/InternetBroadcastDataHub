78ada4418a32995e5bffc46e3da8a7e2bf45d1c0
ho948 <jaeho9948@gmail.com>
2025-02-12 22:31:39 +0900

    feat: 유튜브 최신 인급동 분석용 테이블 생성 쿼리 in superset

--- sql/ddl/create_youtube_latest_table.sql
+++ sql/ddl/create_youtube_latest_table.sql
@@ -1,37 +1,11 @@
--- 테이블 삭제
-DROP TABLE IF EXISTS youtube.youtube_trending_latest_video_with_rank;
-DROP TABLE IF EXISTS youtube.youtube_trending_latest_hourly_all_videos_views_count;
-DROP TABLE IF EXISTS youtube.youtube_trending_latest_hourly_top_10_videos_views_count;
-DROP TABLE IF EXISTS youtube.youtube_trending_latest_hourly_top_1_video_views_count;
-DROP TABLE IF EXISTS youtube.youtube_trending_latest_daily_all_videos_views_count;
-DROP TABLE IF EXISTS youtube.youtube_trending_latest_daily_top_10_videos_views_count;
-DROP TABLE IF EXISTS youtube.youtube_trending_latest_daily_top_1_videos_views_count;
-DROP TABLE IF EXISTS youtube.youtube_trending_latest_weekly_all_videos_views_count;
-DROP TABLE IF EXISTS youtube.youtube_trending_latest_weekly_top_10_videos_views_count;
-DROP TABLE IF EXISTS youtube.youtube_trending_latest_weekly_top_1_videos_views_count;
-DROP TABLE IF EXISTS youtube.youtube_trending_latest_hourly_all_channels_subscribers_count;
-DROP TABLE IF EXISTS youtube.youtube_trending_latest_hourly_top_10_channels_subscribers_count;
-DROP TABLE IF EXISTS youtube.youtube_trending_latest_hourly_top_1_channel_subscribers_count;
-DROP TABLE IF EXISTS youtube.youtube_trending_latest_daily_all_channels_subscribers_count;
-DROP TABLE IF EXISTS youtube.youtube_trending_latest_daily_top_10_channels_subscribers_count;
-DROP TABLE IF EXISTS youtube.youtube_trending_latest_daily_top_1_channels_subscribers_count;
-DROP TABLE IF EXISTS youtube.youtube_trending_latest_weekly_all_channels_subscribers_count;
-DROP TABLE IF EXISTS youtube.youtube_trending_latest_weekly_top_10_channels_subscribers_count;
-DROP TABLE IF EXISTS youtube.youtube_trending_latest_weekly_top_1_channels_subscribers_count;
-DROP TABLE IF EXISTS youtube.youtube_trending_latest_top_1_video_views_count;
-DROP TABLE IF EXISTS youtube.youtube_trending_latest_all_video_time_to_trending;
-DROP TABLE IF EXISTS youtube.youtube_trending_latest_top_10_video_time_to_trending;
-DROP TABLE IF EXISTS youtube.youtube_trending_latest_top_1_video_time_to_trending;
-DROP TABLE IF EXISTS youtube.youtube_trending_latest_channel_subscribers_count;
-DROP TABLE IF EXISTS youtube.youtube_trending_latest_channel_trending_ts;
-DROP TABLE IF EXISTS youtube.youtube_trending_latest_channel_videos_views_count;
-DROP TABLE IF EXISTS youtube.youtube_trending_latest_channel_videos_time_to_trending;
-DROP TABLE IF EXISTS youtube.youtube_trending_latest_channel_videos_rank;
-DROP TABLE IF EXISTS youtube.youtube_trending_latest_channel_trending_count;
+DROP TABLE IF EXISTS youtube.latest_video_with_rank;
+DROP TABLE IF EXISTS youtube.latest_weekly_views_count;
+DROP TABLE IF EXISTS youtube.latest_channel_name_latest;
+DROP TABLE IF EXISTS youtube.latest_channel_video_views_count;
+DROP TABLE IF EXISTS youtube.latest_channel_live_time_to_trending;
 
--- 순위가 포함된 쿼리용 테이블 작성
-CREATE TABLE IF NOT EXISTS youtube.youtube_trending_latest_video_with_rank (
-    id SERIAL,
+
+CREATE TABLE IF NOT EXISTS youtube.latest_video_with_rank (
     rank INT,
     link VARCHAR(255) NOT NULL,
     title VARCHAR(255),
@@ -44,250 +18,33 @@ CREATE TABLE IF NOT EXISTS youtube.youtube_trending_latest_video_with_rank (
     subscribers_count INT,
     channel_img VARCHAR(255),
     execution_ts TIMESTAMP NOT NULL,
-    PRIMARY KEY (id, link, execution_ts)
+    PRIMARY KEY (link, execution_ts)
 );
-
--- 분석용 테이블 생성
--- 시간대별 모든 최신 인기 급상승 영상들의 최고/최저/평균 조회수 테이블 생성
-CREATE TABLE IF NOT EXISTS youtube.youtube_trending_latest_hourly_all_videos_views_count (
-    id SERIAL,
-    ts TIMESTAMP NOT NULL,
+CREATE TABLE IF NOT EXISTS youtube.latest_weekly_views_count (
+    weekday VARCHAR(20) NOT NULL,
     max_views_count INT,
     min_views_count INT,
     avg_views_count INT,
-    PRIMARY KEY (id, ts)
+    PRIMARY KEY (weekday)
 );
--- 시간대별 최신 Top 10 인기 급상승 영상들의 최고/최저/평균 조회수 테이블 생성
-CREATE TABLE IF NOT EXISTS youtube.youtube_trending_latest_hourly_top_10_videos_views_count (
-    id SERIAL,
-    ts TIMESTAMP NOT NULL,
-    max_views_count INT,
-    min_views_count INT,
-    avg_views_count INT,
-    PRIMARY KEY (id, ts)
+CREATE TABLE IF NOT EXISTS youtube.latest_channel_name_latest (
+    channel_link VARCHAR(255) PRIMARY KEY,
+    channel_name VARCHAR(255),
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
 );
--- 시간대별 최신 Top 1 인기 급상승 영상 조회수 테이블 생성
-CREATE TABLE IF NOT EXISTS youtube.youtube_trending_latest_hourly_top_1_video_views_count (
-    id SERIAL,
-    ts TIMESTAMP NOT NULL,
+CREATE TABLE IF NOT EXISTS youtube.latest_channel_video (
+    channel_name VARCHAR(255),
+    video_link VARCHAR(255),
     views_count INT,
-    PRIMARY KEY (id, ts)
-);
--- 일별 최신 모든 인기 급상승 영상들의 최고/최저/평균 조회수 테이블 생성
-CREATE TABLE IF NOT EXISTS youtube.youtube_trending_latest_daily_all_videos_views_count (
-    id SERIAL,
-    date DATE NOT NULL,
-    max_views_count INT,
-    min_views_count INT,
-    avg_views_count INT,
-    PRIMARY KEY (id, date)
-);
--- 일별 최신 Top 10 인기 급상승 영상들의 최고/최저/평균 조회수 테이블 생성
-CREATE TABLE IF NOT EXISTS youtube.youtube_trending_latest_daily_top_10_videos_views_count (
-    id SERIAL,
-    date DATE NOT NULL,
-    max_views_count INT,
-    min_views_count INT,
-    avg_views_count INT,
-    PRIMARY KEY (id, date)
-);
--- 일별 최신 Top 1 인기 급상승 영상들의 최고/최저/평균 조회수 테이블 생성
-CREATE TABLE IF NOT EXISTS youtube.youtube_trending_latest_daily_top_1_videos_views_count (
-    id SERIAL,
-    date DATE NOT NULL,
-    max_views_count INT,
-    min_views_count INT,
-    avg_views_count INT,
-    PRIMARY KEY (id, date)
-);
--- 요일별 최신 모든 인기 급상승 영상들의 최고/최저/평균 조회수 테이블 생성
-CREATE TABLE IF NOT EXISTS youtube.youtube_trending_latest_weekly_all_videos_views_count (
-    id SERIAL,
-    weekday VARCHAR(20) NOT NULL,
-    max_views_count INT,
-    min_views_count INT,
-    avg_views_count INT,
-    PRIMARY KEY (id, weekday)
-);
--- 요일별 최신 Top 10 인기 급상승 영상들의 최고/최저/평균 조회수 테이블 생성
-CREATE TABLE IF NOT EXISTS youtube.youtube_trending_latest_weekly_top_10_videos_views_count (
-    id SERIAL,
-    weekday VARCHAR(20) NOT NULL,
-    max_views_count INT,
-    min_views_count INT,
-    avg_views_count INT,
-    PRIMARY KEY (id, weekday)
-);
--- 요일별 최신 Top 1 인기 급상승 영상들의 최고/최저/평균 조회수 테이블 생성
-CREATE TABLE IF NOT EXISTS youtube.youtube_trending_latest_weekly_top_1_videos_views_count (
-    id SERIAL,
-    weekday VARCHAR(20) NOT NULL,
-    max_views_count INT,
-    min_views_count INT,
-    avg_views_count INT,
-    PRIMARY KEY (id, weekday)
-);
--- 시간대별 최신 모든 인기 급상승에 오른 채널들의 최고/최저/평균 구독자 수 테이블 생성
-CREATE TABLE IF NOT EXISTS youtube.youtube_trending_latest_hourly_all_channels_subscribers_count (
-    id SERIAL,
-    ts TIMESTAMP NOT NULL,
-    max_subscribers_count INT,
-    min_subscribers_count INT,
-    avg_subscribers_count INT,
-    PRIMARY KEY (id, ts)
-);
--- 시간대별 최신 Top 10 인기 급상승에 오른 채널들의 최고/최저/평균 구독자 수 테이블 생성
-CREATE TABLE IF NOT EXISTS youtube.youtube_trending_latest_hourly_top_10_channels_subscribers_count (
-    id SERIAL,
-    ts TIMESTAMP NOT NULL,
-    max_subscribers_count INT,
-    min_subscribers_count INT,
-    avg_subscribers_count INT,
-    PRIMARY KEY (id, ts)
-);
--- 시간대별 최신 Top 1 인기 급상승에 오른 채널의 구독자 수 테이블 생성
-CREATE TABLE IF NOT EXISTS youtube.youtube_trending_latest_hourly_top_1_channel_subscribers_count (
-    id SERIAL,
-    ts TIMESTAMP NOT NULL,
+    rank INT,
     subscribers_count INT,
-    PRIMARY KEY (id, ts)
+    execution_ts TIMESTAMP,
+    PRIMARY KEY (channel_name, video_link, execution_ts)
 );
--- 일별 최신 모든 인기 급상승에 오른 채널들의 최고/최저/평균 구독자 수 테이블 생성
-CREATE TABLE IF NOT EXISTS youtube.youtube_trending_latest_daily_all_channels_subscribers_count (
-    id SERIAL,
-    date DATE NOT NULL,
-    max_subscribers_count INT,
-    min_subscribers_count INT,
-    avg_subscribers_count INT,
-    PRIMARY KEY (id, date)
-);
--- 일별 최신 Top 10 인기 급상승에 오른 채널들의 최고/최저/평균 구독자 수 테이블 생성
-CREATE TABLE IF NOT EXISTS youtube.youtube_trending_latest_daily_top_10_channels_subscribers_count (
-    id SERIAL,
-    date DATE NOT NULL,
-    max_subscribers_count INT,
-    min_subscribers_count INT,
-    avg_subscribers_count INT,
-    PRIMARY KEY (id, date)
-);
--- 일별 최신 Top 1 인기 급상승에 오른 채널들의 최고/최저/평균 구독자 수 테이블 생성
-CREATE TABLE IF NOT EXISTS youtube.youtube_trending_latest_daily_top_1_channels_subscribers_count (
-    id SERIAL,
-    date DATE NOT NULL,
-    max_subscribers_count INT,
-    min_subscribers_count INT,
-    avg_subscribers_count INT,
-    PRIMARY KEY (id, date)
-);
--- 요일별 모든 인기 급상승에 오른 채널들의 최고/최저/평균 구독자 수 테이블 생성
-CREATE TABLE IF NOT EXISTS youtube.youtube_trending_latest_weekly_all_channels_subscribers_count (
-    id SERIAL,
-    weekday VARCHAR(20) NOT NULL,
-    max_subscribers_count INT,
-    min_subscribers_count INT,
-    avg_subscribers_count INT,
-    PRIMARY KEY (id, weekday)
-);
--- 요일별 Top 10 인기 급상승에 오른 채널들의 최고/최저/평균 구독자 수 테이블 생성
-CREATE TABLE IF NOT EXISTS youtube.youtube_trending_latest_weekly_top_10_channels_subscribers_count (
-    id SERIAL,
-    weekday VARCHAR(20) NOT NULL,
-    max_subscribers_count INT,
-    min_subscribers_count INT,
-    avg_subscribers_count INT,
-    PRIMARY KEY (id, weekday)
-);
--- 요일별 Top 1 인기 급상승에 오른 채널들의 최고/최저/평균 구독자 수 테이블 생성
-CREATE TABLE IF NOT EXISTS youtube.youtube_trending_latest_weekly_top_1_channels_subscribers_count (
-    id SERIAL,
-    weekday VARCHAR(20) NOT NULL,
-    max_subscribers_count INT,
-    min_subscribers_count INT,
-    avg_subscribers_count INT,
-    PRIMARY KEY (id, weekday)
-);
--- 1위에 등극한 적이 있는 최신 인기 급상승 영상의 최고/최저/평균 조회수 테이블 생성
-CREATE TABLE IF NOT EXISTS youtube.youtube_trending_latest_top_1_video_views_count (
-    id SERIAL,
-    video_link VARCHAR(255) NOT NULL,
-    max_views_count INT,
-    min_views_count INT,
-    avg_views_count INT,
-    PRIMARY KEY (id, video_link)
-);
--- 업로드 후 최신 모든 인기 급상승에 오른 적이 있는 영상의 모든에 도달까지 걸린 시간 테이블 생성
-CREATE TABLE IF NOT EXISTS youtube.youtube_trending_latest_all_video_time_to_trending (
-    id SERIAL,
-    video_link VARCHAR(255) NOT NULL,
+CREATE TABLE IF NOT EXISTS youtube.latest_channel_live_time_to_trending (
+    channel_name VARCHAR(255),
+    video_link VARCHAR(255),
     time_to_trending INTERVAL,
-    PRIMARY KEY (id, video_link)
-);
--- 업로드 후 최신 Top 10 인기 급상승에 오른 적이 있는 영상의 Top 10 도달까지 걸린 시간 테이블 생성
-CREATE TABLE IF NOT EXISTS youtube.youtube_trending_latest_top_10_video_time_to_trending (
-    id SERIAL,
-    video_link VARCHAR(255) NOT NULL,
-    time_to_trending INTERVAL,
-    PRIMARY KEY (id, video_link)
-);
--- 업로드 후 최신 Top 1 인기 급상승에 오른 적이 있는 영상의 Top 1 도달까지 걸린 시간 테이블 생성
-CREATE TABLE IF NOT EXISTS youtube.youtube_trending_latest_top_1_video_time_to_trending (
-    id SERIAL,
-    video_link VARCHAR(255) NOT NULL,
-    time_to_trending INTERVAL,
-    PRIMARY KEY (id, video_link)
-);
--- 채널별 구독자 수 테이블 생성
-CREATE TABLE IF NOT EXISTS youtube.youtube_trending_latest_channel_subscribers_count (
-    id SERIAL,
-    channel_link VARCHAR(255) NOT NULL,
-    channel_name VARCHAR(255),
-    ts TIMESTAMP NOT NULL,
-    subscribers_count INT,
-    PRIMARY KEY (id, channel_link, ts)
-);
--- 채널별 최신 인기 급상승 시각 테이블 생성
-CREATE TABLE IF NOT EXISTS youtube.youtube_trending_latest_channel_trending_ts (
-    id SERIAL,
-    channel_link VARCHAR(255) NOT NULL,
-    channel_name VARCHAR(255),
-    ts TIMESTAMP NOT NULL,
-    PRIMARY KEY (id, channel_link, ts)
-);
--- 채널별 최신 인기 급상승에 오른 영상들의 최고/최저/평균 조회수 테이블 생성
-CREATE TABLE IF NOT EXISTS youtube.youtube_trending_latest_channel_videos_views_count (
-    id SERIAL,
-    channel_link VARCHAR(255) NOT NULL,
-    channel_name VARCHAR(255),
-    max_views_count INT,
-    min_views_count INT,
-    avg_views_count INT,
-    PRIMARY KEY (id, channel_link)
-);
--- 채널별 최신 인기 급상승에 오른 영상들의 도달까지 걸린 최고/최저/평균 소요 시간 테이블 생성
-CREATE TABLE IF NOT EXISTS youtube.youtube_trending_latest_channel_videos_time_to_trending (
-    id SERIAL,
-    channel_link VARCHAR(255) NOT NULL,
-    channel_name VARCHAR(255),
-    max_time_to_trending INTERVAL,
-    min_time_to_trending INTERVAL,
-    avg_time_to_trending INTERVAL,
-    PRIMARY KEY (id, channel_link)
-);
--- 채널별 최신 인기 급상승 영상들의 최고/최저/평균 순위 테이블 생성
-CREATE TABLE IF NOT EXISTS youtube.youtube_trending_latest_channel_videos_rank (
-    id SERIAL,
-    channel_link VARCHAR(255) NOT NULL,
-    channel_name VARCHAR(255),
-    high_rank INT,
-    low_rank INT,
-    avg_rank FLOAT,
-    PRIMARY KEY (id, channel_link)
-);
--- 채널별 최신 인기 급상승 등극 영상의 수 테이블 생성
-CREATE TABLE IF NOT EXISTS youtube.youtube_trending_latest_channel_trending_count (
-    id SERIAL,
-    channel_link VARCHAR(255) NOT NULL,
-    channel_name VARCHAR(255),
-    trending_count INT,
-    PRIMARY KEY (id, channel_link)
+    PRIMARY KEY (channel_name, video_link)
 );
